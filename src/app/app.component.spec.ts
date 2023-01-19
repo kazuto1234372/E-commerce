e181f788635c6baf8b03f46469eaed1cd5071a12
Zobair Haqyar <ahzobairhaqyar@gmail.com>
2023-01-18 20:21:21 -0600

    First commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,16 +1,14 @@
 import { TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 
 describe('AppComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [
-        RouterTestingModule
-      ],
-      declarations: [
-        AppComponent
-      ],
+      imports: [RouterTestingModule, ProductListComponent],
+      declarations: [AppComponent],
     }).compileComponents();
   });
 
@@ -30,6 +28,8 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('my-app app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain(
+      'my-app app is running!'
+    );
   });
 });
